ed4d0f802438d6008ed994546ac7ee0319418f56
Duncan <duncan@dinegigs.com>
2021-09-19 15:56:24 -0400

    Expense item tag

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,16 @@ import Card from '../UI/Card';
 //import Expenses from './Expenses';
 
 const ExpenseItem = (props) => {
+
+
     return (
-    <Card className="expense-item">
-    <ExpenseDate date={props.date} />
-    <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-    </div>
-    </Card>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
     );
 }
 
