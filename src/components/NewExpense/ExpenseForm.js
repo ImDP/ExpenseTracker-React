d483434a27a0d0d345a5e13496026d82290aef87
Duncan <duncan@dinegigs.com>
2021-09-23 14:01:20 -0400

    created filter by year funtionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,12 +7,12 @@ const ExpenseForm = (props) => {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
-    // eslint-disable-next-line
+ /*   // eslint-disable-next-line
     const [userInput, setUserInput] = useState({
         enteredTitle: '',
         enteredAmount: '',
         enteredDate: '',
-    });
+    }); */
 
     const titleChangeHandler = (e) => {
         setEnteredTitle(e.target.value);
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
 
@@ -70,8 +70,8 @@ const ExpenseForm = (props) => {
                     <label>Amount</label>
                     <input
                         type='number'
-                        min="0.01"
-                        step="0.01"
+                        min='0.01'
+                        step='0.01'
                         value={enteredAmount}
                         onChange={amountChangeHandler} />
                 </div>
@@ -79,15 +79,15 @@ const ExpenseForm = (props) => {
                     <label>Date</label>
                     <input
                         type='date'
-                        min="2019-01-01"
-                        max="2024-12-31"
+                        min='2019-01-01'
+                        max='2022-12-31'
                         value={enteredDate}
                         onChange={dateChangeHandler} />
                 </div>
             </div>
-            <div className="new-expense__actions">
+            <div className='new-expense__actions'>
                 <button
-                    type="submit">Add Expense
+                    type='submit'>Add Expense
                 </button>
             </div>
         </form>
